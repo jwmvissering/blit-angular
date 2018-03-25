ee686591b1350ff5f535c8c65012e201392a0022
Jamon Vissering <jamonvissering@gmail.com>
2018-03-25 13:53:18 +0200

    Loading spinner + code improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule }   from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
-import {AppRoutingModule} from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { HeaderComponent } from './header/header.component';
-import { SearchResultsComponent } from './search-results/search-results.component';
-import { AppService } from './app.service';
-import { PhotoDetailsComponent } from './photo-details/photo-details.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomepageComponent } from "./homepage/homepage.component";
+import { HeaderComponent } from "./header/header.component";
+import { SearchResultsComponent } from "./search-results/search-results.component";
+import { AppService } from "./app.service";
+import { PhotoDetailsComponent } from "./photo-details/photo-details.component";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import {  MatSidenavModule} from "@angular/material/sidenav";
 
 @NgModule({
   imports: [
     BrowserModule,
+    NoopAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    MatProgressSpinnerModule,
+    MatSidenavModule
   ],
   declarations: [
     AppComponent,
@@ -28,4 +34,4 @@ import { PhotoDetailsComponent } from './photo-details/photo-details.component';
   providers: [AppService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
